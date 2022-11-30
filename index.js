f33feaf9803aa667621f15b1b0e34072647746f7
zaidkhan1123 <zk117854@gmail.com>
2022-11-30 19:14:10 +0500

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const Port  = process.env.Port || 3000
 app.use(express.json());
 app.use(express.urlencoded((extended=true)));
 ////////////////////////////////////// Routes ///////////////////////////////////////////////////
@@ -34,6 +34,6 @@ db.once("connected",()=>{
 
 
 
-app.listen(3000,()=>{
+app.listen(Port,()=>{
     console.log("server is running");
 })
